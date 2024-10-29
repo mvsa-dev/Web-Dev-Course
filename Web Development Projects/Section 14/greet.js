50a53b52f325a983d1a303091492ac80e9947a6b
Mvsa-dev <musadlongolo.dev@gmail.com>
2024-10-29 12:46:23 +0200

    Upload of Section 14 greet.js complete

--- Web Development Projects/Section 14/greet.js
+++ Web Development Projects/Section 14/greet.js
@@ -1,9 +1,6 @@
 function greet(name) {
-    let nameUpper = name.toUpperCase();
-    let nameFirst = name[0];
+    let nameFirst = name[0].toUpperCase();
     let nameRest = name.slice(1);
-    let nameGreet = nameFirst + nameRest;
-    console.log("Hello " + nameGreet);
+    console.log("Hello " + nameFirst + nameRest);
 }
-
-greet("Musa_Dev");
+greet("musa");
