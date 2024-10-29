48e3a25407ce16dffe694621c93085c4fa39e090
Mvsa-dev <musadlongolo.dev@gmail.com>
2024-10-29 20:04:26 +0200

    Update of bottles.js for loop

--- Web Development Projects/Section 14/bottles.js
+++ Web Development Projects/Section 14/bottles.js
@@ -1,18 +1,15 @@
 function bottles() {
     let count = 99;
-    while (count > 1) {
-        let wallCount = count - 1;
+    for (i = 1; i < 99; i++) {
+        wallCount = count - 1;
         console.log(count + " bottles of beer on the wall, " + count + " bottles of beer.");
         console.log("Take one down and pass it around, " + wallCount + " bottles of beer on the wall.");
         count--;
-        if (count == 1){
-            console.log("1 bottle of beer on the wall, 1 bottle of beer.");
-            console.log("Take one down and pass it around, no more bottles of beer on the wall.");
-            console.log("No more bottles of beer on the wall, no more bottles of beer.");
-            console.log("Go to the store and buy some more, 99 bottles of beer on the wall.");
-        }
     }
-        
+    console.log("1 bottle of beer on the wall, 1 bottle of beer.");
+    console.log("Take one down and pass it around, no more bottles of beer on the wall.");
+    console.log("No more bottles of beer on the wall, no more bottles of beer.");
+    console.log("Go to the store and buy some more, 99 bottles of beer on the wall.");
 }
 
 bottles();
