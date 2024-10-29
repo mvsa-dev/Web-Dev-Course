6801eb1138eae025b5540a59b85282bba6315f9e
Mvsa-dev <musadlongolo.dev@gmail.com>
2024-10-29 15:19:28 +0200

    Update of bmi.js if/else

--- Web Development Projects/Section 14/bmi.js
+++ Web Development Projects/Section 14/bmi.js
@@ -1,7 +1,24 @@
-function bmi(weight, height){
+/*BMI <18.5, the output should be: "Your BMI is <bmi>, so you are underweight."
+
+BMI 18.5-24.9, the output should be "Your BMI is <bmi>, so you have a normal weight."
+
+BMI >24.9, the output should be "Your BMI is <bmi>, so you are overweight." */
+
+
+
+
+
+function bmiCalc(weight, height){
     let h = Math.pow(height, 2);
-    let bmiCalc = Math.round(weight / h);
-    console.log(bmiCalc);
+    let bmi = Math.round(weight / h);
+
+    if (bmi < 18.5) {
+        console.log("Your BMI is " + bmi + ",  you are underweight.");
+    } else if (bmi > 24.9) {
+        console.log("Your BMI is " + bmi + ",  you are overweight.");
+    } else {
+        console.log("Your BMI is " + bmi + ", so you are normal weight.");
+    }
 }
 
-bmi(88, 1.88);
+bmiCalc(88, 1.87);
