af0b925db8a538cabee8f4c60676e0771cee58b3
Mvsa-dev <musadlongolo.dev@gmail.com>
2024-10-31 11:21:06 +0200

    Upload of fibonacci.js project complete

--- Web Development Projects/Section 14/fibonacci.js
+++ Web Development Projects/Section 14/fibonacci.js
@@ -1,8 +1,26 @@
-function fib(){
+function fib(n){
     let fibonacci = [];
-    let count = 0;
-    for (i = 0; i < 100; i++){
-
-
-    }
+    let num = 0;
+    let num1 = num + 1;
+    let num2;
+    if (n == 1) {
+        fibonacci.push(num);
+        return fibonacci;
+    } else if (n == 2) {
+        fibonacci.push(num);
+        fibonacci.push(num1);
+        return fibonacci;
+    } else {
+        fibonacci.push(num);
+        fibonacci.push(num1);
+        for (j = 2; j < n; j++){
+            num2 = num + num1;
+            fibonacci.push(num2);
+            num = fibonacci[fibonacci.length - 2];
+            num1 = fibonacci[fibonacci.length - 1];
+        }
+        return fibonacci;
+    }   
 }
+
+fib(7);
