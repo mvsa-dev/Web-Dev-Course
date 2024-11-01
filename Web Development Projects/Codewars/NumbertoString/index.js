26be3ae0580e6c3ff36cc03fb842c30562eb4a8f
Mvsa-dev <musadlongolo.dev@gmail.com>
2024-11-01 23:40:11 +0200

    Mods to fibonacci.js + Created Codewars folder

---
+++ Web Development Projects/Codewars/NumbertoString/index.js
@@ -0,0 +1,10 @@
+/*We need a function that can transform a number (integer) into a string.
+123  --> "123"
+999  --> "999"
+-100 --> "-100"
+What ways of achieving this do you know?*/
+function numToString(num) {
+  let str = String(num);
+  console.log(str);
+  
+numToString(5);
--- Web Development Projects/Section 14/fibonacci.js
+++ Web Development Projects/Section 14/fibonacci.js
@@ -1,22 +1,22 @@
-function fib(n){
-    let fibonacci = [];
-    let num = 0;
-    let num1 = num + 1;
-    let num2;
-    if (n == 1) {
-        fibonacci.push(num);
-    } else if (n == 2) {
-        fibonacci.push(num);
-        fibonacci.push(num1);
-    } else {
-        fibonacci.push(num);
-        fibonacci.push(num1);
-        for (j = 2; j < n; j++){
-            num2 = num + num1;
-            fibonacci.push(num2);
-            num = fibonacci[fibonacci.length - 2];
-            num1 = fibonacci[fibonacci.length - 1];
-        }
-        return fibonacci;
+function fib(n) {
+  let fibonacci = [];
+  let num = 0;
+  let num1 = num + 1;
+  let num2;
+  if (n == 1) {
+    fibonacci.push(num);
+  } else if (n == 2) {
+    fibonacci.push(num);
+    fibonacci.push(num1);
+  } else {
+    fibonacci.push(num);
+    fibonacci.push(num1);
+    for (j = 2; j < n; j++) {
+      num2 = num + num1;
+      fibonacci.push(num2);
+      num = fibonacci[fibonacci.length - 2];
+      num1 = fibonacci[fibonacci.length - 1];
     }
+    return console.log(fibonacci);
+  }
 }
