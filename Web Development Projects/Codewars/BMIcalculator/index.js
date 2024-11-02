fd08334c4d58f3bb3614e03ae26af68086406339
Mvsa-dev <musadlongolo.dev@gmail.com>
2024-11-02 11:45:11 +0200

    Upload of BMIcalculator file complete

--- Web Development Projects/Codewars/BMIcalculator/index.js
+++ Web Development Projects/Codewars/BMIcalculator/index.js
@@ -5,7 +5,7 @@ function bmi(weight, height) {
   else if (value <= 18.5) return value + " Underweight";
   else return value + " Normal";
 }
-console.log(bmi(100, 1.8));
+bmi(100, 1.8);
 /*Write function bmi that calculates body mass index (bmi = weight / height2).
     if bmi <= 18.5 return "Underweight"
     if bmi <= 25.0 return "Normal"
