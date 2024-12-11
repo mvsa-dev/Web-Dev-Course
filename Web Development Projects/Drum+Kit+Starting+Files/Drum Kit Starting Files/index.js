9ec937a4c6335f226b637a8a640d0dae76dfcf78
Musa <musadlongolo.dev@gmail.com>
2024-12-12 01:43:51 +0200

    DrumKit project finished

--- Web Development Projects/Drum+Kit+Starting+Files/Drum Kit Starting Files/index.js
+++ Web Development Projects/Drum+Kit+Starting+Files/Drum Kit Starting Files/index.js
@@ -40,3 +40,41 @@ function playAudio() {
       break;
   }
 }
+
+document.addEventListener("keydown", keyAudio);
+
+function keyAudio(event) {
+  let key = event.key;
+  switch (key) {
+    case "w":
+      let tom1 = new Audio("./sounds/tom-1.mp3");
+      tom1.play();
+      break;
+    case "a":
+      let tom2 = new Audio("./sounds/tom-2.mp3");
+      tom2.play();
+      break;
+    case "s":
+      let tom3 = new Audio("./sounds/tom-3.mp3");
+      tom3.play();
+      break;
+    case "d":
+      let tom4 = new Audio("./sounds/tom-4.mp3");
+      tom4.play();
+      break;
+    case "j":
+      let kick = new Audio("./sounds/kick-bass.mp3");
+      kick.play();
+      break;
+    case "k":
+      let snare = new Audio("./sounds/snare.mp3");
+      snare.play();
+      break;
+    case "l":
+      let crash = new Audio("./sounds/crash.mp3");
+      crash.play();
+      break;
+    default:
+      break;
+  }
+}
