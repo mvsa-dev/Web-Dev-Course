fa49dc7608da17fd397789b6696adb41998b170b
Musa <musadlongolo.dev@gmail.com>
2024-12-22 06:22:17 +0200

    Simon Game current

--- Web Development Projects/Simon+Game+Challenge+Starting+Files/Simon Game Challenge Starting Files/game.js
+++ Web Development Projects/Simon+Game+Challenge+Starting+Files/Simon Game Challenge Starting Files/game.js
@@ -1,10 +1,48 @@
+//Array gamePattern to store the game pattern
 let gamePattern = [];
-
-let buttonColours = ["red", "blue", "green", "yellow"];
-let randomChosenColour = buttonColours[randomNumber];
-gamePattern.push(randomChosenColour);
-
+//Array userPattern to store the user pattern
+let userPattern = [];
+//function nextSequence to generate next color and add it to the gamePattern array
+$(document).keydown(launchGame);
+// Functions
 function nextSequence() {
   let randomNumber = Math.floor(Math.random() * 4);
-  return randomNumber;
+  let buttonColours = ["red", "blue", "green", "yellow"];
+  let randomChosenColour = buttonColours[randomNumber];
+  //add the randomChosenColour to the gamePattern array
+  gamePattern.push(randomChosenColour);
+  //flash the button with the randomChosenColour
+  $("#" + randomChosenColour)
+    .fadeIn(100)
+    .fadeOut(100)
+    .fadeIn(100);
+  //play the sound with the randomChosenColour
+  playSound(randomChosenColour);
+}
+
+function launchGame() {
+  nextSequence();
+  userClick();
+}
+
+function gameLevel() {
+  let level = gamePattern.length + 1;
+  $("h1").text("Level " + level);
+}
+
+function startText() {
+  $("h1").text("Press A Key to Start");
+}
+
+function playSound(randomChosenColour) {
+  let audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+  audio.play();
+}
+
+function userClick() {
+  $(".btn").click(userInput);
+}
+
+function userInput(event) {
+  console.log(event);
 }
