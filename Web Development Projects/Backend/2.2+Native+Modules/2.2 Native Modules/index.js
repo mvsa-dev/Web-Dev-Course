f16d67d81928c72efd8ae87397b79477961e6709
Musa <musadlongolo.dev@gmail.com>
2025-01-03 08:17:49 +0200

    Completed 2.2 files

---
+++ Web Development Projects/Backend/2.2+Native+Modules/2.2 Native Modules/index.js
@@ -0,0 +1,15 @@
+const { ifError } = require("assert");
+const { clear } = require("console");
+const fs = require("fs");
+
+/*fs.writeFile("message.txt", "Trust in the Lord!", (err) => {
+  if (err) throw err;
+  console.log("The file has been saved!");
+});*/
+
+fs.readFile("message.txt", "utf8", callback);
+
+function callback(err, data) {
+  if (err) throw err;
+  console.log(data);
+}
