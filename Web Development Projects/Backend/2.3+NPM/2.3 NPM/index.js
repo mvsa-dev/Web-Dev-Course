99635dad5bb40050c3531824f8aa248da9fd24f9
Musa <musadlongolo.dev@gmail.com>
2025-01-04 10:29:46 +0200

    Completed 2.3 NPM and included NPM and JSON

---
+++ Web Development Projects/Backend/2.3+NPM/2.3 NPM/index.js
@@ -0,0 +1,7 @@
+/*import generateName from "sillyname";
+var sillyName = generateName();
+console.log(`My name is ${sillyName}.`);*/
+
+import { randomSuperhero } from "superheroes";
+var SuperheroName = randomSuperhero();
+console.log(`Do not fear! ${SuperheroName} is here!`);
