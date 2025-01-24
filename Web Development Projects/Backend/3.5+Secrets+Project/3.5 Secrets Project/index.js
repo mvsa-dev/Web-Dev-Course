3a100d2a96d60abcd099adce63870343258f3d1a
Musa <musadlongolo.dev@gmail.com>
2025-01-24 02:31:38 +0200

    Added secrets project complete

--- Web Development Projects/Backend/3.5+Secrets+Project/3.5 Secrets Project/index.js
+++ Web Development Projects/Backend/3.5+Secrets+Project/3.5 Secrets Project/index.js
@@ -1,3 +1,31 @@
-//To see how the final website should work, run "node solution.js".
-//Make sure you have installed all the dependencies with "npm i".
-//The password is ILoveProgramming
+import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import bodyParser from "body-parser";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+const port = 3000;
+const correctPass = "Bro";
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.post("/check", passCheck);
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}.`);
+});
+
+function passCheck(req, res) {
+  let passIn = req.body.password;
+  if (passIn === correctPass) {
+    res.sendFile(__dirname + "/public/secret.html");
+  } else {
+    res.sendFile(__dirname + "/public/index.html");
+  }
+}
