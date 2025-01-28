a48d06664f65f012c5367f86ae1bfdfda15de200
Musa <musadlongolo.dev@gmail.com>
2025-01-28 12:01:53 +0200

    4.2 EJS Passing Data files complete

--- Web Development Projects/Backend/4.2+Passing+Data/4.2 Passing Data/index.js
+++ Web Development Projects/Backend/4.2+Passing+Data/4.2 Passing Data/index.js
@@ -1,14 +1,30 @@
 import express from "express";
 import bodyParser from "body-parser";
+import ejs from "ejs";
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {});
+app.get("/", (req, res) => {
+  res.render("./index.ejs", {
+    showH: true,
+    origin: "Enter your name below 👇",
+  });
+});
 
-app.post("/submit", (req, res) => {});
+app.post("/submit", (req, res) => {
+  //Add body input to variables
+  let nameFirst = req.body.fName;
+  let nameLast = req.body.lName;
+  //Get number of chars in the input
+  let charLength = nameFirst.length + nameLast.length;
+  res.render("./index.ejs", {
+    showH: false,
+    num: charLength,
+  });
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
