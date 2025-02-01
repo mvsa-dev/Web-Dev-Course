5f2dbd076a7824448092213d5bf4baf48549ef07
Musa <musadlongolo.dev@gmail.com>
2025-02-01 11:16:19 +0200

    Capstone 3 Project files

---
+++ Web Development Projects/Backend/Capstone+Project+3/Capstone Project 3/index.js
@@ -0,0 +1,13 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
