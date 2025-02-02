32f371878f8afeb3f81ffc6ec55db34cd64f260d
Musa <musadlongolo.dev@gmail.com>
2025-02-02 23:28:20 +0200

    Added changes to Capstone Project 3

--- Web Development Projects/Backend/Capstone+Project+3/Capstone Project 3/index.js
+++ Web Development Projects/Backend/Capstone+Project+3/Capstone Project 3/index.js
@@ -8,6 +8,10 @@ app.use(express.static("public"));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
