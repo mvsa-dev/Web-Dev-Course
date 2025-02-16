141f1514cfc9e4699a9e44e764cdcd45e659598f
Musa <musadlongolo.dev@gmail.com>
2025-02-16 20:57:30 +0200

    Added Comment section and post of comment.

--- Web Development Projects/Backend/Capstone+Project+3/Capstone Project 3/index.js
+++ Web Development Projects/Backend/Capstone+Project+3/Capstone Project 3/index.js
@@ -9,8 +9,20 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.render("index.ejs");
+  res.render("index.ejs", {
+    showH: true,
+  });
 });
+
+app.post("/submit", (req, res) => {
+  let blogPost = req.body.content;
+  res.render("./index.ejs", {
+    showH: false,
+    userComment: blogPost,
+  });
+});
+
+app.delete("/delete");
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
