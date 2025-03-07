b00f3333fe70ff1be7cba005374f8e1139792048
Musa <musadlongolo.dev@gmail.com>
2025-03-07 06:48:29 +0200

    Added current project progress

--- Web Development Projects/Backend/Capstone+Project+3/Capstone Project 3/index.js
+++ Web Development Projects/Backend/Capstone+Project+3/Capstone Project 3/index.js
@@ -3,26 +3,29 @@ import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
+let postData = []; // Stores user posts in an array of objects.
 
 app.use(express.static("public"));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
+  let blogPost = "";
   res.render("index.ejs", {
-    showH: true,
+    userComment: blogPost,
   });
 });
 
 app.post("/submit", (req, res) => {
   let blogPost = req.body.content;
-  res.render("./index.ejs", {
-    showH: false,
+  res.render("index.ejs", {
     userComment: blogPost,
   });
 });
 
-app.delete("/delete");
+app.delete("/delete", (req, res) => {
+  res.render("index.ejs", {});
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
