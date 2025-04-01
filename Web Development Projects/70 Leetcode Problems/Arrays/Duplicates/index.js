d021860675474e20034a28579b4da93d0a76a841
Musa <musadlongolo.dev@gmail.com>
2025-04-01 19:07:34 +0200

    finish array question using sets

--- Web Development Projects/70 Leetcode Problems/Arrays/Duplicates/index.js
+++ Web Development Projects/70 Leetcode Problems/Arrays/Duplicates/index.js
@@ -1,4 +1,9 @@
-// Given an integer array num, return true if any value appears at least twice in the array, and return false if every element is distinct.
+// Given an integer array num,
+// return true if any value appears at least twice in the array,
+// and return false if every element is distinct.
+
+// Using for loops...(less efficient)
+/*
 const num = [4, 6, 3, 3, 2];
 
 const appearsMoreThanOnce = (arr) => {
@@ -11,6 +16,21 @@ const appearsMoreThanOnce = (arr) => {
     }
   }
   console.log(distinct);
+};
+
+appearsMoreThanOnce(num);
+*/
+
+// Using sets...(more efficient)
+const num = [1, 3, 5, 3, 4, 1, 7, 2];
+
+const appearsMoreThanOnce = (arr) => {
+  const newNum = new Set(arr);
+  if (newNum === arr) {
+    console.log("false");
+  } else {
+    console.log("true");
+  }
 };
 
 appearsMoreThanOnce(num);
